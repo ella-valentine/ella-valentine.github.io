903ad6ef13f9bddfcbebdaec860544eb829bbabd
Alexander Jones <spiderland@outlook.com>
2023-01-10 00:29:46 +0800

    Add nav, fades, corridor content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,48 @@
 $(document).ready(init)
 
 function init() {
-    $("body").removeClass("hidden");
+    // $("body").removeClass("hidden");
+    setupRouter();
+}
+
+function setupRouter() {
+
+    const opacityTransitionLength = 1000;
+    const removeFromDomDelay = 50;
+    const fadeInDelay = 150;
+
+    const fadeOut = (elem) => {
+        elem.addClass("hidden");
+        setTimeout(() => {
+            elem.addClass("removed");
+        }, opacityTransitionLength + removeFromDomDelay)
+    }
+
+    const fadeIn = (elem) => {
+
+        setTimeout(() => {
+            elem.removeClass("removed");
+        }, fadeInDelay)
+        setTimeout(() => {
+            elem.removeClass("hidden");
+        }, fadeInDelay + removeFromDomDelay)
+    }
+
+    $(".routable li a").on('click', function(e) {
+        e.preventDefault();
+
+        const query = this.hash;
+        const elem = $(query);
+        if (!elem.length) {
+            return
+        }
+
+        const shownPage = $(".gallery-section:not(.hidden.removed)")
+        if (shownPage.is(elem)) {
+            return;
+        }
+
+        fadeOut(shownPage);
+        fadeIn(elem, fadeInDelay);
+    })
 }
