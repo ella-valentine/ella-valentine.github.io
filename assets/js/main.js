68cb30d378a987f2b1e121a5d13018f44928e693
Alexander Jones <spiderland@outlook.com>
2023-01-10 22:45:54 +0800

    Sidebar complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,31 +3,38 @@ $(document).ready(init)
 function init() {
     // $("body").removeClass("hidden");
     setupRouter();
+    setupSidebarButtons();
+}
+
+const defaultOpacityTransitionLength = 1000;
+const defaultRemoveFromDomDelay = 50;
+const defaultFadeInDelay = 150;
+
+const fadeOut = (elem, opacityTransitionLength, removeFromDomDelay) => {
+
+    var otl = opacityTransitionLength ?? defaultOpacityTransitionLength
+    var rfdd = removeFromDomDelay ?? defaultRemoveFromDomDelay;
+
+    elem.addClass("hidden");
+    setTimeout(() => {
+        elem.addClass("removed");
+    }, otl + rfdd)
+}
+
+const fadeIn = (elem, fadeInDelay, removeFromDomDelay) => {
+
+    var fid = fadeInDelay ?? defaultFadeInDelay;
+    var rfdd = removeFromDomDelay ?? defaultRemoveFromDomDelay;
+
+    setTimeout(() => {
+        elem.removeClass("removed");
+    }, fid)
+    setTimeout(() => {
+        elem.removeClass("hidden");
+    }, fid + rfdd)
 }
 
 function setupRouter() {
-
-    const opacityTransitionLength = 1000;
-    const removeFromDomDelay = 50;
-    const fadeInDelay = 150;
-
-    const fadeOut = (elem) => {
-        elem.addClass("hidden");
-        setTimeout(() => {
-            elem.addClass("removed");
-        }, opacityTransitionLength + removeFromDomDelay)
-    }
-
-    const fadeIn = (elem) => {
-
-        setTimeout(() => {
-            elem.removeClass("removed");
-        }, fadeInDelay)
-        setTimeout(() => {
-            elem.removeClass("hidden");
-        }, fadeInDelay + removeFromDomDelay)
-    }
-
     $(".routable li a").on('click', function(e) {
         e.preventDefault();
 
@@ -44,7 +51,47 @@ function setupRouter() {
             return;
         }
 
-        fadeOut(shownPage);
-        fadeIn(elem);
+        closeModal();
+        fadeOut(shownPage, 500, 10);
+        fadeIn(elem, 510, 10);
     })
+}
+
+function setupSidebarButtons() {
+
+    var sidebarOpen = false;
+
+    $("#sidebar-control").on("click", function (e) {
+
+
+        if (!sidebarOpen) {
+            openModal()
+        } else {
+            closeModal();
+        }
+
+        // $(".sidebar-overlay").removeClass("hidden");
+    })
+}
+
+function openModal() {
+
+    var hidden = $("svg.hidden");
+    var shown = $("svg:not(.hidden)")
+    hidden.removeClass("hidden")
+    shown.addClass("hidden");
+    $(".sidebar-content").removeClass("tray-hidden");
+    fadeIn($(".sidebar-overlay"), 0, 20);
+    sidebarOpen = true;
+}
+
+function closeModal() {
+
+    var hidden = $("svg.hidden");
+    var shown = $("svg:not(.hidden)")
+    hidden.removeClass("hidden")
+    shown.addClass("hidden");
+    $(".sidebar-content").addClass("tray-hidden");
+    fadeOut($(".sidebar-overlay"), 500, 20);
+    sidebarOpen = false;
 }
